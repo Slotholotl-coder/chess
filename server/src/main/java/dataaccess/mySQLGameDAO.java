d76e383f260104e04d758d0cf77ddaa78f2dc433
Slotholotl-coder <2015eli.sam@gmail.com>
2024-11-02 16:47:56 -0600

    insertUser

--- server/src/main/java/dataaccess/mySQLGameDAO.java
+++ server/src/main/java/dataaccess/mySQLGameDAO.java
@@ -1,4 +1,29 @@
 package dataaccess;
 
-public class mySQLGameDAO {
+import model.GameData;
+
+import java.util.Collection;
+import java.util.List;
+
+public class mySQLGameDAO implements GameDAO {
+
+    @Override
+    public void insertGame(GameData game) {
+
+    }
+
+    @Override
+    public GameData getGame(int gameNum) {
+        return null;
+    }
+
+    @Override
+    public Collection<GameData> getAllGames() {
+        return List.of();
+    }
+
+    @Override
+    public void clear() {
+
+    }
 }
--- server/src/main/java/dataaccess/mySQLUserDAO.java
+++ server/src/main/java/dataaccess/mySQLUserDAO.java
@@ -1,4 +1,28 @@
 package dataaccess;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class mySQLUserDAO {
+    public void insertUser(User user) {
+        String INSERT_USERS_SQL = "INSERT INTO users (id, pwd, fullname, email) VALUES (?, ?, ?, ?);";
+
+        try (Connection connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)) {
+
+            preparedStatement.setString(1, user.getId());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setString(3, user.getFullname());
+            preparedStatement.setString(4, user.getEmail());
+
+            int rowAffected = preparedStatement.executeUpdate();
+            if (rowAffected > 0) {
+                System.out.println("User inserted successfully!");
+            }
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+    }
 }
