a91c5b92116cbf5b5e93766cc631e066f044eec9
Eli Anderson <elisam03@byu.edu>
2025-10-29 00:08:31 -0600

    clear test are complete

--- server/src/test/java/service/ClearTests.java
+++ server/src/test/java/service/ClearTests.java
@@ -1,4 +1,18 @@
 package service;
 
-public class ClearTests {
+import dataaccess.DataAccessException;
+import io.javalin.http.Context;
+import org.junit.jupiter.api.Test;
+import server.ClearAllDataHandler;
+
+public class ClearTests extends ServiceTest{
+
+    @Test
+    void clearTest() throws DataAccessException {
+        userDAO.clear();
+        authDAO.clear();
+        gameDAO.clear();
+        assert (gameDAO.getAllGames().isEmpty());
+    }
+
 }
