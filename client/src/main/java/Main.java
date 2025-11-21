ce4fa48bc6d9839f0acb22f1d17132b5f4f95836
Eli Anderson <elisam03@byu.edu>
2025-11-20 19:56:03 -0700

    Printing board set up

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,10 +1,18 @@
 import chess.*;
 import serverFacade.ServerFacade;
+import ui.BoardPrinter;
 import ui.PreLoginUI;
 import server.Server;
 
 public class Main {
     public static void main(String[] args) {
+
+        BoardPrinter boardPrinter = new BoardPrinter();
+
+        boardPrinter.printBoard(new ChessGame(), ChessGame.TeamColor.BLACK);
+        System.out.println();
+        boardPrinter.printBoard(new ChessGame(), ChessGame.TeamColor.WHITE);
+
         ServerFacade serverFacade = new ServerFacade("localhost:8080");
         Server server = new Server();
         server.run(8080);
