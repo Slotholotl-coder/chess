867d0cc7065847715d37a4f62b0be734fbc432db
Eli Anderson <elisam03@byu.edu>
2025-11-23 16:15:23 -0700

    Refactoring to make client and server modules independent

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -4,9 +4,6 @@ import ui.PreLoginUI;
 public class Main {
     public static void main(String[] args) {
         int port = 8080;
-        if (args.length == 1){
-            port = Integer.parseInt(args[0]);
-        }
 
         ServerFacade serverFacade = new ServerFacade(port);
 
