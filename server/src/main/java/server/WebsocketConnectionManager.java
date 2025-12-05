d1f5538177a9cb656ae839e55ce6769765121cf5
Eli Anderson <elisam03@byu.edu>
2025-12-04 17:32:04 -0700

    Creating websock connection manager

--- server/src/main/java/server/WebsocketConnectionManager.java
+++ server/src/main/java/server/WebsocketConnectionManager.java
@@ -4,6 +4,7 @@ import org.eclipse.jetty.websocket.api.Session;
 import websocket.messages.ServerMessage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -11,7 +12,13 @@ public class WebsocketConnectionManager {
     public final ConcurrentHashMap<Integer, List<Session>> connections = new ConcurrentHashMap<>();
 
     public void add(int gameID, Session session) {
-        connections.get(gameID).add(session);
+        try {
+            connections.get(gameID).add(session);
+        } catch (Exception e) {
+            List<Session> newGameSession = new ArrayList<>();
+            newGameSession.add(session);
+            connections.put(gameID, newGameSession);
+        }
     }
 
     public void remove(int gameID, Session session) {
