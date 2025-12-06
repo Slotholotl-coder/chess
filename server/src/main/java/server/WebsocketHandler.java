8e16422698c0b1c3b69dee0ed3323912a6e97753
Eli Anderson <elisam03@byu.edu>
2025-12-05 20:10:08 -0700

    refactor to change observer actions and server message for observers

--- server/src/main/java/server/WebsocketHandler.java
+++ server/src/main/java/server/WebsocketHandler.java
@@ -62,11 +62,8 @@ public class WebsocketHandler implements WsConnectHandler, WsMessageHandler, WsC
 
         try {
             ServerMessage serverMessage = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION);
-            if (userGameCommand.getTeamColor() != null) {
                 serverMessage.setMessage(userGameCommand.getUsername() + " joined the game as " + userGameCommand.getTeamColor());
-            } else {
                 serverMessage.setMessage(userGameCommand.getUsername() + " is observing");
-            }
             websocketConnectionManager.broadcast(userGameCommand.getGameID(), wsMessageContext.session, serverMessage);
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -94,7 +91,7 @@ public class WebsocketHandler implements WsConnectHandler, WsMessageHandler, WsC
         } catch (IOException e) {
             throw new RuntimeException(e);
         } catch (DataAccessException e) {
-            throw new RuntimeException(e);
+
         }
     }
 
